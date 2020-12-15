da3e336e5302f9c789c167ee99383f821afd91e1
Redwolf-Studio <worakan9012@gmail.com>
2020-12-15 22:33:18 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,93 @@
 /*
-KiTSUNE Speed shutter Tester Beta V0.1
+KiTSUNE Speed shutter Tester Beta V0.1 [Build test]
 By REDWOLF
+
+used ATMEGA328P ONLY!!
+Libraries requirement
+  - Adafruit BusIO
+  - Adafruit GFX Library
+  - Adafruit SSD1306
 */
 
 #include <Arduino.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+#include <Wire.h>
+#include <SPI.h>
+
+uint8_t resetButt = 3, upButt = 4, downButt = 5;
+
 unsigned long StartTime, StopTime, DeltaTime;
 float ShutterTime1,ShutterTime2;
+
+boolean SettingPage;
 volatile boolean shutterFlag = false, signalReading = false;
 
 void setup() {
   Serial.begin(9600);
 
+  //Power_control -> on
+  DDRD |= (1 << DDD6); //Set PD6 to output
+  PORTD |= (1 << PD6); //PD6 HIGH
+
+
   //Pin setup
   //interrupts INT0
   DDRD &= ~(1 << DDD2);     //Set PD2 pin  to INPUT
   PORTD |= (1 << PORTD2);   //Enable pull-up
-  //Pin 3 (Reset)
-  DDRD &= ~(1 << DDD3);     //Set PD3 pin  to INPUT
-  PORTD |= (1 << PORTD3);   //Enable pull-up
+  
 
   //Interrupts setup 
   EICRA |= (1 << ISC00);    //Set INT0 to trigger on any logic change
   EIMSK |= (1 << INT0);     //Enable INT0
   sei();                    //Enable Interrupts
 
+
+  pinMode(resetButt, INPUT_PULLUP);   // Reset | Enter
+  pinMode(upButt, INPUT_PULLUP);      // Up ⬆
+  pinMode(downButt, INPUT_PULLUP);    // Down ⬇
+
 }//Void setup
 
 void loop() {
 
+  //Setting
+  while(digitalRead(resetButt) == LOW) {
+    uint16_t counter = millis();
+    if(counter >= 2000) {
+      SettingPage = !SettingPage;
+    }
+  }
+
+  if(SettingPage == true) {
+    //Display setting page
+    uint8_t Page = 0;
+    if(digitalRead(downButt) == LOW) {
+      Page++;
+      delay(5);
+    }
+    if(digitalRead(upButt) == LOW) {
+      Page--;
+      delay(5);
+    }
+
+    if(Page > 1 || Page == 255) {
+        Page = 0;
+    }
+    
+    //scene
+    switch (Page) {
+      //Auto shuttdown time
+      case 0:
+      break;
+      //Dim display
+      case 1:
+      break;
+    }
+  }
+
+  //Reset Value
   if(digitalRead(3) == LOW) {
     //RESET logic and value
     shutterFlag = false;
@@ -47,34 +104,8 @@ void loop() {
     DeltaTime = (StopTime - StartTime);
     ShutterTime1 = (float)DeltaTime/1000000;
     ShutterTime2 = 1/ShutterTime1;
-
-    //Serial Debug
-    Serial.println("---------------------------");
     
-    Serial.print("Shutter : ");
-    if(ShutterTime1 >= 1) {
-      Serial.println(ShutterTime1, 5);
-    }else if(ShutterTime1 < 1) {
-      Serial.print("1/");
-      Serial.println(ShutterTime2, 5);
-    }
 
-
-
-    Serial.println("\n");
-    Serial.print("Start Time : ");
-    Serial.println(StartTime);
-    Serial.print("Stop Time  : ");
-    Serial.println(StopTime);
-    Serial.print("Microsec. : ");
-    Serial.println(DeltaTime);
-    Serial.print("Shutter Time : ");
-    Serial.println(ShutterTime1, 5);
-    Serial.print("Shutter 1/ : ");
-    Serial.println(ShutterTime2, 5);
-    Serial.println("---------------------------");    
-
-    
     //RESET logic and value
     shutterFlag = false;
     signalReading = false;
